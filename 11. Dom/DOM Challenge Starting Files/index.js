020dc351a7bdf3c5c849f533fa693155a217f451
Nafis Ahmad <nafis.ahmad0087@gmail.com>
2023-01-27 15:26:44 +0530

    Document object model

---
+++ 11. Dom/DOM Challenge Starting Files/index.js
@@ -0,0 +1,9 @@
+document.getElementsByTagName('li')[2].innerHTML = 'Nafis';
+
+document.querySelector('h1').innerHTML = '<em>Hello</em>';
+// document.querySelector('h1').innerText = '<em>Hello</em>';
+
+document.querySelector('li').style.color = 'red';
+document.querySelector('h1').style.fontSize = '32rem';
+document.querySelector('button').style.backgroundColor = 'yellow';
+document.body.style.backgroundColor = 'black';
