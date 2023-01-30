2a0f793ca35ed315f59613638feeb7f85f7b21d6
Nafis Ahmad <nafis.ahmad0087@gmail.com>
2023-01-30 20:27:49 +0530

    update frontend

--- 12. The Dice Game/Dicee Challenge - Starting Files/index1.js
+++ 12. The Dice Game/Dicee Challenge - Starting Files/index1.js
@@ -1,26 +1,26 @@
-// First Player
+// first player
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
-var diceImage1 = 'dice' + randomNumber1 + '.png';
-var randomImageSource1 = 'images/' + diceImage1;
+var randomDiceImage1 = 'dice1' + randomNumber1 + '.png';
+var randomImageSource1 = 'images/' + randomDiceImage1;
 
-var image1 = document.querySelectorAll('img')[0];
-image1.setAttribute('src', randomImageSource1);
+var diceImage1 = document.querySelectorAll('img')[0];
+diceImage1.setAttribute('src', randomImageSource1);
 
-// 2nd Player
+// second Player
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-var diceImage2 = 'dice' + randomNumber2 + '.png';
-var randomImageSource2 = 'images/' + diceImage2;
+var randomDiceImage2 = 'dice' + randomNumber2 + '.png';
+var randomImageSource2 = 'images/' + randomDiceImage2;
 
-var image2 = document.querySelectorAll('img')[1];
-image2.setAttribute('src', randomImageSource2);
+var diceImage2 = document.querySelectorAll('img')[1];
+diceImage2.setAttribute('src', randomImageSource2);
 
 // who wins
 if (randomNumber1 > randomNumber2) {
-  document.querySelector('h1').innerText = 'Player 1 wins';
+  document.querySelector('h1').innerText = 'Player 1 Wins';
 } else if (randomNumber1 < randomNumber2) {
-  document.querySelector('h1').innerText = 'Player 2 wins';
+  document.querySelector('h1').innerText = 'Player 2 Wins';
 } else {
-  document.querySelector('h1').innerText = `It's a tie`;
+  document.querySelector('h1').innerText = "It's a Tie !";
 }
