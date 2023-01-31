950e7cf92c46e486c82ceafbf49dc5245976ae86
Nafis Ahmad <nafis.ahmad0087@gmail.com>
2023-01-31 16:38:44 +0530

    advance js

---
+++ 13. Advanced Javascript/calculator.js
@@ -0,0 +1,17 @@
+// calculator
+function add(num1, num2) {
+  return num1 + num2;
+}
+function sub(num1, num2) {
+  return num1 + num2;
+}
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+function div(num1, num2) {
+  return num1 / num2;
+}
+
+function calculator(num1, num2, op) {
+  return op(num1, num1);
+}
