a479e3bfdd6ab499f641ab3c52ef93ff56db84d0
Nafis Ahmad <nafis.ahmad0087@gmail.com>
2023-02-02 12:34:37 +0530

    Node Modules

---
+++ 18. Node JS/Node Modules/index.js
@@ -0,0 +1,9 @@
+var superHeroes = require('superheroes');
+var superVillains = require('superheroes');
+
+var mySuperheroName = superHeroes.random();
+var mySuperVillainName = superVillains.random();
+
+console.log('SuperHero: ' + mySuperheroName);
+console.log('SuperVillain: ' + mySuperVillainName);
+  
