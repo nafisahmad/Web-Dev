f22cd43650d28d7eb22794912bb4503251be5497
Nafis Ahmad <nafis.ahmad0087@gmail.com>
2023-02-03 15:11:13 +0530

    Weather App

---
+++ 20. API's/weatherApp/index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const https = require('https');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/', function (req, res) {
+  req.body.cityName;
+  console.log('Post recieved');
+
+  //   logic + api
+  const api = '5c7083bed1dcec5e6f7a03aedd9ae4e0';
+  const unit = 'metric';
+  const city = req.body.cityName;
+  const url =
+    'https:/api.openweathermap.org/data/2.5/weather?q=' +
+    city +
+    '&appid=' +
+    api +
+    '&units=' +
+    unit;
+
+  https.get(url, function (response) {
+    console.log(response.statusCode);
+
+    response.on('data', function (data) {
+      const weatherData = JSON.parse(data);
+      // console.log(weatherData);
+      const temp = weatherData.main.temp;
+      const weatherDescription = weatherData.weather[0].description;
+      const icon = weatherData.weather[0].icon;
+      const imageURL = 'http://openweathermap.org/img/wn/' + icon + '@2x.png';
+
+      res.write(
+        '<h1>Temperature in ' + city + ' is : ' + temp + ' &#8451; </h1>'
+      );
+
+      res.write('<h2>The scene is currently : ' + weatherDescription + '</h2>');
+      res.write('<img src=' + imageURL + '>');
+      res.send();
+      // console.log(weatherDescription);
+    });
+  });
+
+  //   res.send('Server Running');
+});
+
+app.listen(3000, function () {
+  console.log('server is running at port 3000');
+});
