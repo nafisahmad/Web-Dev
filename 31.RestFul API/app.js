42a6f6c9080ecd1ff82b34ad1176364c7c75f589
Nafis Ahmad <nafis.ahmad0087@gmail.com>
2023-02-12 15:33:25 +0530

    CRUD With Postman

--- 31.RestFul API/app.js
+++ 31.RestFul API/app.js
@@ -17,11 +17,57 @@ mongoose.connect(MONGO_URL, () => {
 });
 
 // Create Item schema
-const articlesSchema = {
+const articleSchema = {
   title: String,
   content: String,
 };
 
+// Name of model = Article
+const Article = mongoose.model('Article', articleSchema);
+
+app.get('/articles', function (req, res) {
+  Article.find(function (err, foundArticles) {
+    // console.log(foundArticles);
+    if (!err) {
+      res.send(foundArticles);
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+// post articles from the DB
+app.post('/articles', function (req, res) {
+  // console.log(req.body.title);
+  // console.log(req.body.content);
+
+  const newArticle = new Article({
+    title: req.body.title,
+    content: req.body.content,
+  });
+
+  // save new artcle with error handling
+  newArticle.save(function (err) {
+    if (!err) {
+      res.send('Successfully added a new article');
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+// delete all article
+app.delete('/articles', function (req, res) {
+  Article.deleteMany(function (err) {
+    if (!err) {
+      res.send('Successfully deleted all articles');
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+// Server
 app.listen(3000, function () {
   console.log('server started at port: 3000');
 });
