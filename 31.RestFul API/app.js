6755b616210aa6f46b86d88be23b767cd645460b
Nafis Ahmad <nafis.ahmad0087@gmail.com>
2023-02-12 17:29:08 +0530

    Complete RESTFUL APi

--- 31.RestFul API/app.js
+++ 31.RestFul API/app.js
@@ -25,47 +25,136 @@ const articleSchema = {
 // Name of model = Article
 const Article = mongoose.model('Article', articleSchema);
 
-app.get('/articles', function (req, res) {
-  Article.find(function (err, foundArticles) {
-    // console.log(foundArticles);
-    if (!err) {
-      res.send(foundArticles);
-    } else {
-      res.send(err);
-    }
-  });
-});
+// // ROUTE REQUEST targetting all articels
+app
+  .route('/articles')
+  .get(function (req, res) {
+    Article.find(function (err, foundArticles) {
+      // console.log(foundArticles);
+      if (!err) {
+        res.send(foundArticles);
+      } else {
+        res.send(err);
+      }
+    });
+  })
+  .post(function (req, res) {
+    // console.log(req.body.title);
+    // console.log(req.body.content);
+    const newArticle = new Article({
+      title: req.body.title,
+      content: req.body.content,
+    });
 
-// post articles from the DB
-app.post('/articles', function (req, res) {
-  // console.log(req.body.title);
-  // console.log(req.body.content);
-
-  const newArticle = new Article({
-    title: req.body.title,
-    content: req.body.content,
+    // save new artcle with error handling
+    newArticle.save(function (err) {
+      if (!err) {
+        res.send('Successfully added a new article');
+      } else {
+        res.send(err);
+      }
+    });
+  })
+  .delete(function (req, res) {
+    Article.deleteMany(function (err) {
+      if (!err) {
+        res.send('Successfully deleted all articles');
+      } else {
+        res.send(err);
+      }
+    });
   });
 
-  // save new artcle with error handling
-  newArticle.save(function (err) {
-    if (!err) {
-      res.send('Successfully added a new article');
-    } else {
-      res.send(err);
-    }
-  });
-});
+// app.get('/articles', function (req, res) {
+//   Article.find(function (err, foundArticles) {
+//     // console.log(foundArticles);
+//     if (!err) {
+//       res.send(foundArticles);
+//     } else {
+//       res.send(err);
+//     }
+//   });
+// });
 
-// delete all article
-app.delete('/articles', function (req, res) {
-  Article.deleteMany(function (err) {
-    if (!err) {
-      res.send('Successfully deleted all articles');
-    } else {
-      res.send(err);
-    }
+// Post articles from the DB
+// app.post('/articles', function (req, res) {
+//   // console.log(req.body.title);
+//   // console.log(req.body.content);
+
+//   const newArticle = new Article({
+//     title: req.body.title,
+//     content: req.body.content,
+//   });
+
+//   // Save new artcle with error handling
+//   newArticle.save(function (err) {
+//     if (!err) {
+//       res.send('Successfully added a new article');
+//     } else {
+//       res.send(err);
+//     }
+//   });
+// });
+
+// Delete all article
+// app.delete('/articles', function (req, res) {
+//   Article.deleteMany(function (err) {
+//     if (!err) {
+//       res.send('Successfully deleted all articles');
+//     } else {
+//       res.send(err);
+//     }
+//   });
+// });
+
+// ROUTE REQUEST targettting specific Article
+app
+  .route('/articles/:articleTitle')
+  .get(function (req, res) {
+    Article.findOne(
+      { title: req.params.articleTitle },
+      function (err, foundArticle) {
+        if (foundArticle) {
+          res.send(foundArticle);
+        } else {
+          res.send('No Articles matching the title was found ');
+        }
+      }
+    );
+  })
+  .put(function (req, res) {
+    Article.update(
+      { title: req.params.articleTitle },
+      { $set: { title: req.body.title, content: req.body.content } },
+      { overwrite: true },
+      function (err) {
+        if (!err) {
+          res.send('Successfully updated Article');
+        }
+      }
+    );
+  })
+  .patch(function (req, res) {
+    Article.update(
+      { title: req.params.articleTitle },
+      { $set: req.body },
+      function (err) {
+        if (!err) {
+          res.send('Succesfully updated the article');
+        }
+        // else {
+        //   res.send(err);
+        // }
+      }
+    );
+  })
+  .delete(function (req, res) {
+    Article.deleteOne({ title: req.params.articleTitle }, function (err) {
+      if (!err) {
+        res.send('Deleted successfully');
+      }
+    });
   });
-});
 
 // Server
 app.listen(3000, function () {
