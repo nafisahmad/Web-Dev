23cbbe94382065a59cca630578ca5d68830aaa26
Nafis Ahmad <nafis.ahmad0087@gmail.com>
2023-02-14 15:46:05 +0530

    Google auth

--- 32.Security/Secrets - Starting Code/app.js
+++ 32.Security/Secrets - Starting Code/app.js
@@ -13,6 +13,9 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocalMongoose = require('passport-local-mongoose');
+// GOOGLE AUTH
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 
 const app = express();
 // console.log(process.env.SECRET);
@@ -56,6 +59,7 @@ const userSchema = new mongoose.Schema({
 // });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model('User', userSchema);
 passport.use(User.createStrategy());
@@ -63,10 +67,39 @@ passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      callbackURL: 'http://localhost:3000/auth/google/secrets',
+      userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo',
+    },
+
+    function (accessToken, refreshToken, profile, cb) {
+      User.findOrCreate({ googleId: profile.id }, function (err, user) {
+        return cb(err, user);
+      });
+    }
+  )
+);
+
 // ROUTES
 app.get('/', function (req, res) {
   res.render('home');
 });
+app.get('/auth/google', function (req, res) {
+  passport.authenticate('google', { scope: ['profile'] });
+});
+app.get(
+  '/auth/google/secrets',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  }
+);
+
 app.get('/login', function (req, res) {
   res.render('login');
 });
