75b89a2234773a8f861d3e9fd4dba50169afe96c
Nafis Ahmad <nafis.ahmad0087@gmail.com>
2023-02-15 13:51:41 +0530

    complete chapter

--- 32.Security/Secrets - Starting Code/app.js
+++ 32.Security/Secrets - Starting Code/app.js
@@ -49,6 +49,7 @@ mongoose.connect(process.env.MONGO_URL + 'userDB', function () {
 const userSchema = new mongoose.Schema({
   email: String,
   password: String,
+  googleId: String,
 });
 
 // MONGOOSE-ENCRYPTION FOR PASSWORD
@@ -64,8 +65,14 @@ userSchema.plugin(findOrCreate);
 const User = new mongoose.model('User', userSchema);
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
 
 passport.use(
   new GoogleStrategy(
@@ -88,15 +95,16 @@ passport.use(
 app.get('/', function (req, res) {
   res.render('home');
 });
-app.get('/auth/google', function (req, res) {
-  passport.authenticate('google', { scope: ['profile'] });
-});
+app.get(
+  '/auth/google',
+  passport.authenticate('google', { scope: ['profile'] })
+);
 app.get(
   '/auth/google/secrets',
   passport.authenticate('google', { failureRedirect: '/login' }),
   function (req, res) {
     // Successful authentication, redirect home.
-    res.redirect('/');
+    res.redirect('/secrets');
   }
 );
 
@@ -107,11 +115,43 @@ app.get('/register', function (req, res) {
   res.render('register');
 });
 app.get('/secrets', function (req, res) {
+  User.find({ secret: { $ne: null } }, function (err, foundUsers) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUsers) {
+        res.render('secrets', { usersWithSecrets: foundUsers });
+      }
+    }
+  });
+});
+
+app.get('/submit', function (req, res) {
   if (req.isAuthenticated()) {
-    res.render('secrets');
+    res.render('submit');
   } else {
     res.redirect('/login');
   }
+});
+
+app.post('/submit', function (req, res) {
+  const submittedSecret = req.body.secret;
+
+  //Once the user is authenticated and their session gets saved, their user details are saved to req.user.
+  // console.log(req.user.id);
+
+  User.findById(req.user.id, function (err, foundUser) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        foundUser.secret = submittedSecret;
+        foundUser.save(function () {
+          res.redirect('/secrets');
+        });
+      }
+    }
+  });
 });
 
 app.get('/logout', function (req, res, next) {
@@ -122,6 +162,7 @@ app.get('/logout', function (req, res, next) {
     res.redirect('/');
   });
 });
+
 app.post('/register', function (req, res) {
   // BCRYPt
   // bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
@@ -179,6 +220,7 @@ app.post('/login', function (req, res) {
     username: req.body.username,
     password: req.body.password,
   });
+
   req.login(user, function (err) {
     if (err) {
       console.log(err);
